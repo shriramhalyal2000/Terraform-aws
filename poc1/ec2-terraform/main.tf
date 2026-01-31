408bdb4941fcd3e3375f78031ca65868b8d6dbd0
shriramhalyal2000 <shriramhalyal48@gmail.com>
2026-01-31 23:39:45 +0530

    verion, var, main and ignoretf file update

---
+++ poc1/ec2-terraform/main.tf
@@ -0,0 +1,13 @@
+provider aws{
+    region = ap-south-1
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "tf-vpc"
+  }
+}
