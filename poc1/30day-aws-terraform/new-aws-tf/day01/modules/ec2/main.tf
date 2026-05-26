0dac097ea1a65981f216f09fe7311f68b329c082
shriramhalyal2000 <shriramhalyal48@gmail.com>
2026-05-26 23:26:33 +0530

    adding  tf infra files

---
+++ poc1/30day-aws-terraform/new-aws-tf/day01/modules/ec2/main.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "amazon_linux_2023" {
+    most_recent = true
+    owners = [amazon]
+
+    filter {
+        name = "name"
+        values = ["al2023-ami-2023*-x86_64"]
+    }
+    filter{
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+
+resource "aws_instance" "test_vm" {
+    ami = data.aws_ami.amazon_linux_2023.id
+    instance_type = t3.micro 
+}
