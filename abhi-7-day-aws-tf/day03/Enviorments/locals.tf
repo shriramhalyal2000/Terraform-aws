49e5b7cd03a97d9098cab1667ec4fcc4a64a8ef0
shriramhalyal2000 <shriramhalyal48@gmail.com>
2026-06-18 19:12:07 +0530

    Ec2 instace module execution infra code

---
+++ abhi-7-day-aws-tf/day03/Enviorments/locals.tf
@@ -0,0 +1,4 @@
+locals {
+  aws_security_group_env_name = "sg-http-ssh"
+  ebs_vol_env_name            = "ebs_vol_attach_name"
+}
---
+++ abhi-7-day-aws-tf/day03/modules/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2-instance" {
+  source                  = "../Enviorments"
+  instance_ami            = "ami-0521cb2d60cfbb1a6"
+  instance_region         = "us-east-1"
+  instance_type           = "t3.micro"
+  instance_key_name       = "us-east-1"
+  ip_protocol             = "tcp"
+  ebs_instance_vol_type   = "gp3"
+  ebs_instance_vol_size   = 15
+  to_port_http            = 80
+  from_port_http          = 80
+  cidr_ipv4               = "0.0.0.0/0"
+  to_port_ssh             = 22
+  from_port_ssh           = 22
+  aws_security_group_rule = "http and ssh "
+  ebs_attach_dir          = "/dev/sdh"
+
+}
