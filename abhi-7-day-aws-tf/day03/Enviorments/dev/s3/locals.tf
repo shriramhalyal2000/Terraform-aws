168f55b4bf4657acab0b5ef84d611006ac5ee2e6
shriramhalyal2000 <shriramhalyal48@gmail.com>
2026-06-19 23:29:17 +0530

    added dev s3 bucket speced resources

---
+++ abhi-7-day-aws-tf/day03/Enviorments/dev/s3/locals.tf
@@ -0,0 +1,4 @@
+locals{
+    bucket_name_tag = "s3-bucket-module"
+    bucket_enviornment_tag = "Dev"
+}
---
+++ abhi-7-day-aws-tf/day03/Enviorments/dev/s3/var.tf
@@ -0,0 +1,4 @@
+variable "aws_s3_bucket_name"{
+    description = "S3 bucket name"
+    type = string
+}
