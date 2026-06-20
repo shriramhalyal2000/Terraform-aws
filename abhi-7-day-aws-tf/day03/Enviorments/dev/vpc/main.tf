a56b113627a2078c56c62059efffd37663595ef1
shriramhalyal2000 <shriramhalyal48@gmail.com>
2026-06-20 23:52:19 +0530

    added provider and vpc res script file

---
+++ abhi-7-day-aws-tf/day03/Enviorments/dev/vpc/main.tf
@@ -0,0 +1,29 @@
+resource "aws_vpc" "my_task_1"{
+    cidr_block = var.cidr_block
+
+    tags={
+        Name = local.vpc_name
+        Enviornment = local.vpc_enviornment
+    }
+}
+
+resource "aws_internet_gateway" "my_task_1_igw"{
+    vpc_id = aws_vpc.my_task_1.id
+
+    tags={
+        Name = "${local.vpc_name}-igw"
+    }
+}
+
+# create aws_route table 
+# create route table association
+
+resource "aws_route_table" "my_task_1_rt"{
+    vpc_id = aws_vpc.my_task_1.id
+
+    route = var.raoute # string []
+
+    tags={
+        Name = local.vpc_name
+    }
+}
