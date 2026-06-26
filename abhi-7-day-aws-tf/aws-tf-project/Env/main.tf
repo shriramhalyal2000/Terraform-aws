50fe3f18cbac3df8b0f84b5f0d4f768715caff2e
shriramhalyal2000 <shriramhalyal48@gmail.com>
2026-06-26 23:25:16 +0530

    changed base64encode string for usedata1.sh file

--- abhi-7-day-aws-tf/aws-tf-project/Env/main.tf
+++ abhi-7-day-aws-tf/aws-tf-project/Env/main.tf
@@ -16,6 +16,6 @@ module "vpc" {
   instance_key           = "us-east-1"
   instance_type          = "t2.micro"
   instance_iam_role      = "ec2-ssm-role"
-  user_data1             = filebase64("../Module/userdata1.sh")
-  user_data2             = filebase64("../Module/userdata2.sh")
+  user_data1             = base64decode(file("../userdata1.sh"))
+  user_data2             = base64decode(file("../userdata2.sh"))
 }
