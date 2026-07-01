d020a8b31c3a69a4620f80db06aadc035ba14cf1
shriramhalyal2000 <shriramhalyal48@gmail.com>
2026-07-01 15:20:34 +0530

    updated lifecycle rules

--- abhi-7-day-aws-tf/tf-syntax/life_cycle_rule.tf
+++ abhi-7-day-aws-tf/tf-syntax/life_cycle_rule.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "webserver" {
-    ami = ""
+    ami = "ami-0521cb2d60cfbb1a6"
     count = 1
     key_name = "us-east-1"
 
@@ -8,8 +8,8 @@ resource "aws_instance" "webserver" {
         Enviornment = "Task"
     }  
     lifecycle {
-      create_before_destroy = 
-      prevent_destroy = 
+      create_before_destroy = false
+      prevent_destroy = false
       ignore_changes = [ tags, key_name ]
       replace_triggered_by = [ aws_instance ]
     }
