b019ec10b23737dbffacd8a79d9c61840a77c521
shriramhalyal2000 <shriramhalyal48@gmail.com>
2026-08-30 14:24:38 +0530

    changed rt names

--- abhi-7-day-aws-tf/mini-projects/eks/mod/local.tf
+++ abhi-7-day-aws-tf/mini-projects/eks/mod/local.tf
@@ -3,7 +3,7 @@ locals{
     sub1_name = "eks-sbn1"
     sub2_name = "eks-sbn2"
     igw_name = "eks-igw"
-    rt_name = "eks-rt"
+    rt_name = "eks-pvt-rt"
     eks_public_sbn_name = "eks_public_subnet"
-    eks_public_rt = "eks_public_sbn_rt"
+    eks_public_rt = "eks-public-rt"
 }
